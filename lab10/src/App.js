c1767da9f40db733dc939611bfd08661ad751e12
andrew0149 <andrew-m0r0z@mail.ru>
2024-05-15 19:53:33 +0500

    Add missing semicolon

--- lab10/src/App.js
+++ lab10/src/App.js
@@ -1,7 +1,7 @@
 import MainBlock from "./components/mainBlock";
 import icon1 from "./images/icon1.png";
 import icon2 from "./images/icon2.png";
-import icon3 from "./images/icon3.png"
+import icon3 from "./images/icon3.png";
 
 function App() {
   return (
